0396b79cf4088e281203ecb790a70bf70ac73772
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-05 23:59:21 +0530

    adding index.html

---
+++ index.js
@@ -0,0 +1,4 @@
+const hourEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
