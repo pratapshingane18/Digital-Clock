d40c98605f1430ae6e241545b3a1cef6a94483e7
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-06 00:00:54 +0530

    Upading time

--- index.js
+++ index.js
@@ -8,4 +8,21 @@ function updateClock() {
     let m = new Date().getMinutes();
     let s = new Date().getSeconds();
     let ampm = "AM";
-  
+    if (h > 12) {
+        h = h - 12;
+        ampm = "PM";
+      }
+    
+      h = h < 10 ? "0" + h : h;
+      m = m < 10 ? "0" + m : m;
+      s = s < 10 ? "0" + s : s;
+    
+      hourEl.innerText = h;
+      minuteEl.innerText = m;
+      secondEl.innerText = s;
+      ampmEl.innerText = ampm;
+      setTimeout(() => {
+        updateClock();
+      }, 1000);
+    }
+      
