2fa0f42dce253419b076388754d3cf8e4fb47a24
pratapshingane18 <sushrutrao465@gmail.com>
2023-01-05 23:59:57 +0530

    getting the dates

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ const hourEl = document.getElementById("hour");
 const minuteEl = document.getElementById("minutes");
 const secondEl = document.getElementById("seconds");
 const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+    let h = new Date().getHours();
+    let m = new Date().getMinutes();
+    let s = new Date().getSeconds();
+    let ampm = "AM";
+  
